fe8745a62ff54957a4c6c83e9c4667a6e0b2cf26
ayerein <ayelenreinglestein@gmail.com>
2022-12-08 15:03:42 -0300

    Entrega Websockets

--- utils/sockets/index.js
+++ utils/sockets/index.js
@@ -10,16 +10,33 @@ class Socket{
         this.io = new SocketIO(http)
         this.productos = []
         this.mensajes = []
-        this.usuarios = []
+        this.usuarios = null
     }
     init(){
         try {
             this.io.on('connection', socket =>{
-                this.io.sockets.emit("fillP", this.productos)
+                this.io.sockets.emit("fillP", this.mensajes)
+                this.io.sockets.emit("fillProd", this.productos)
                 console.log("Usuario conectado.")
+                this.io.sockets.emit("init", this.mensajes)
+                this.io.sockets.emit("init", this.productos)
+
+                socket.on("agregarUsuario", user => {
+                    this.usuarios = user
+                    console.log("nuevo usuario", user.email)
+                })
+
+                socket.on("nuevoMensaje", data => {
+                    let today = new Date();
+                    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+"  "+today.getHours() + ":" + today.getMinutes()
+                    let nuevoMensaje = `<b style="color:blue">${this.usuarios.email}</b> [${date}]: <i style="color:green">${data}</i>`
+                    this.mensajes.push(nuevoMensaje)
+                    this.io.sockets.emit("fillP", this.mensajes)
+                })
+
                 socket.on("nuevoProducto", prod =>{
                     this.productos.push(prod)
-                    this.io.sockets.emit("fillP", this.productos)
+                    this.io.sockets.emit("fillProd", this.productos)
                 })
             })
         } catch (error) {
